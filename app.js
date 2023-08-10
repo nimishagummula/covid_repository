42cc62bf8baa4ddcbec277799c66bd7df2498f78
nimishagummula <nimishagummula@gmail.com>
2023-08-10 06:25:04 +0000

    solution

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+const path = require("path");
+const dbPath = path.join(__dirname, covid19India.db);
+const app = express();
+let db = null;
+const initializeDbAndServer = async () => {
+  try {
+    db = await open({
+      fileName: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error:${e.message}`);
+  }
+};
+initializeDbAndServer();
